b768cc12bddd1da0c28674df8dc1a59b72dcbd15
Pine <rajiohousou@gmail.com>
2023-06-27 00:27:19 +0900

    introduce a progress bar

--- main.cpp
+++ main.cpp
@@ -3,11 +3,32 @@
 
 int LOOP_TIME = 10;
 double sum = 0;
+bool huge = false;
+
+int checkProgress(int count, int loop_time)
+{
+    if (count % (loop_time / 10) == 0)
+    {
+        char test[] = "123456789";
+        std::cout << "\r" << std::flush;
+        for (int j = 0; j < count / (loop_time / 10); j++)
+        {
+            std::cout << test[j] << std::flush;
+        }
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
-    if (argc >= 2){
+    if (argc >= 2)
+    {
         LOOP_TIME = atoi(argv[1]);
+    }
+
+    if (LOOP_TIME >= 100000000)
+    {
+        huge = true;
     }
 
     std::cout << LOOP_TIME << "回実行します" << std::endl;
@@ -20,8 +41,14 @@ int main(int argc, char *argv[])
         sum += item;
         // std::cout << child << " / " << mother << std::endl;
         // std::cout << i << std::endl;
+
+        if (huge == true && (i % (LOOP_TIME / 10)) == 0)
+        {
+            checkProgress(i, LOOP_TIME);
+        }
     }
 
+    std::cout << std::endl;
     double PI = sum * 4;
     std::cout << "PI = " << PI << std::endl;
 }
